31f681969198398d84ef3fa4677cddb8315fe6b4
austin ce <austin.cawley@gmail.com>
2020-04-02 14:40:35 -0400

    ci: make tiller-start posix

--- scripts/tiller-start.sh
+++ scripts/tiller-start.sh
@@ -2,20 +2,18 @@
 
 set -e
 
-if [[ -n "${1+x}" ]]; then
-  TILLER_BIN=${1}
-else
-  TILLER_BIN=tiller
-fi
-
+TILLER_BIN=${1:-"tiller"}
 echo "Using tiller binary: ${TILLER_BIN}"
-
 
 TILLER_NAMESPACE=${TILLER_NAMESPACE:-"tiller"}
 
 # Even though we'll be running tiller locally, it still needs a namespace
-kubectl get namespace "$TILLER_NAMESPACE" || kubectl create namespace "$TILLER_NAMESPACE"
+if ! kubectl get namespace "$TILLER_NAMESPACE"; then
+  echo "Creating namespace: $TILLER_NAMESPACE"
+  kubectl create namespace "$TILLER_NAMESPACE"
+fi
 
 export TILLER_NAMESPACE
-${TILLER_BIN} -listen=localhost:44134 -storage=secret -alsologtostderr >/dev/null 2>&1
+echo "Starting tiller server"
+"$TILLER_BIN" -listen=localhost:44134 -storage=secret -alsologtostderr >/dev/null 2>&1
 
