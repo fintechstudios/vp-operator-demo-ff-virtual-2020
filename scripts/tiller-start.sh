423d27ef756d434db115dcad26aee3566a38429e
austin ce <austin.cawley@gmail.com>
2020-04-04 10:08:08 -0400

    ci: tiller scripts

--- scripts/tiller-start.sh
+++ scripts/tiller-start.sh
@@ -1,19 +1,8 @@
 #!/usr/bin/env bash
 
-set -e
+export TILLER_NAMESPACE=${TILLER_NAMESPACE:-"tiller"}
+TILLER=${TILLER:-"tiller"}
+echo "Using tiller binary '${TILLER}' and namespace '${TILLER_NAMESPACE}'"
 
-TILLER_BIN=${1:-"tiller"}
-echo "Using tiller binary: ${TILLER_BIN}"
-
-TILLER_NAMESPACE=${TILLER_NAMESPACE:-"tiller"}
-
-# Even though we'll be running tiller locally, it still needs a namespace
-if ! kubectl get namespace "$TILLER_NAMESPACE"; then
-  echo "Creating namespace: $TILLER_NAMESPACE"
-  kubectl create namespace "$TILLER_NAMESPACE"
-fi
-
-export TILLER_NAMESPACE
-echo "Starting tiller server"
-"$TILLER_BIN" -listen=localhost:44134 -storage=secret -alsologtostderr >/dev/null 2>&1
+${TILLER} -listen=localhost:44134 -storage=secret -alsologtostderr >/dev/null 2>&1
 
